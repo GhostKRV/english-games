4992067e3be138d3b0d90d2a6cddc4ebcf0422a3
GhostRV <andrey95100@gmail.com>
2020-09-11 15:32:20 +0300

    add http bin page

---
+++ src/components/SelectFormInput/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import { Paper, TextField, MenuItem } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const requestMethods = [
+  {
+    value: 'GET',
+    label: 'GET',
+  },
+  {
+    value: 'POST',
+    label: 'POST',
+  },
+  {
+    value: 'PUT',
+    label: 'PUT',
+  },
+  {
+    value: 'DELETE',
+    label: 'DELETE',
+  },
+  {
+    value: 'PATCH',
+    label: 'PATCH',
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    maxWidth: '1360px',
+    padding: '10px',
+    margin: '0 auto',
+    boxShadow: 'none',
+  },
+  parametersField: {
+    width: '100%',
+  },
+  sendButton: {
+    width: '100%',
+    marginTop: '12px',
+  },
+}));
+
+const SelectFormInput = (props) => {
+  const classes = useStyles();
+  const {
+    value = null, //method type
+    onChange,
+    label = null,
+    helperText = null,
+    options = requestMethods,
+  } = props;
+
+  return (
+    <Paper className={classes.paper}>
+      <TextField
+        select
+        label={label} //method
+        value={value}
+        onChange={onChange} //handleCurrencyChange
+        helperText={helperText} //Please select request method
+        className={classes.parametersField}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    </Paper>
+  );
+};
+
+export default SelectFormInput;
--- src/pages/HttpBin/index.js
+++ src/pages/HttpBin/index.js
@@ -1,7 +1,153 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import {
+  Container,
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  CircularProgress,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import axios from 'axios';
+
+import FormInput from '../../components/FormInput';
+import SelectFormInput from '../../components/SelectFormInput';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    maxWidth: '1360px',
+    padding: '10px',
+    margin: '0 auto',
+    boxShadow: 'none',
+  },
+  sendButton: {
+    width: '100%',
+    marginTop: '14px',
+    height: '42px',
+  },
+}));
+
+// ----------------------- EXAMPLES
+const exampleJson = `{"name":"Molecule Man","age":29,"secretIdentity":"Dan Jukes","powers":["Radiation resistance","Turning tiny","Radiation blast"]}`;
+// --------------------------END EXAMPLES
 
 const HttpBin = () => {
-  return <h1>HttpBin</h1>;
+  const classes = useStyles();
+  const [methodType, setCurrency] = useState('GET');
+  const [queryParameters, setQueryParameters] = useState('get');
+  const [bodyParameters, setBodyParameters] = useState(exampleJson);
+  const [response, setResponse] = useState({});
+  const [responseIsLoad, setResponseIsLoad] = useState(true);
+
+  const createRequest = () => {
+    const axiosConfig = {
+      method: methodType,
+      baseURL: `https://httpbin.org/`,
+      data: JSON.parse(bodyParameters),
+      url: queryParameters,
+    };
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        axios(axiosConfig)
+          .then(({ data }) => {
+            resolve(data);
+          })
+          .catch((error) => {
+            resolve(error);
+          });
+      }, 3000);
+    });
+  };
+
+  return (
+    <Container>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5">HTTP BIN</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <SelectFormInput
+            value={methodType}
+            label="Method"
+            onChange={(event) => {
+              setCurrency(event.target.value);
+              setQueryParameters(event.target.value.toLowerCase());
+            }}
+            helperText="Please select request method"
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <FormInput
+            label="URL query parameters "
+            helperText="Query parameters. For example: /get, /post, /path, /delete More details on https://httpbin.org/"
+            value={queryParameters}
+            onChange={(event) => {
+              setQueryParameters(event.target.value);
+            }}
+            error={queryParameters.length === 0}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              className={classes.sendButton}
+              onClick={() => {
+                setResponseIsLoad(false);
+                if (queryParameters.length !== 0)
+                  createRequest().then((data) => {
+                    setResponseIsLoad(true);
+                    setResponse(data);
+                  });
+              }}
+            >
+              {responseIsLoad ? (
+                <Typography>SEND</Typography>
+              ) : (
+                <CircularProgress color="inherit" size={20} />
+              )}
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <FormInput
+            label="Query body parameters"
+            helperText="Body parameters"
+            rows={6}
+            rowsMax={15}
+            value={bodyParameters}
+            onChange={(event) => {
+              setBodyParameters(event.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormInput
+            label="Server Response "
+            rows={6}
+            rowsMax={250}
+            value={JSON.stringify(response, undefined, 2)}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default HttpBin;
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     '& > * + *': {
+//       marginLeft: theme.spacing(2),
+//     },
+//   },
+// }));
